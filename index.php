cb9a0ae600c304a28c89fe1d5d922ae444d63349
Mariprasad Ramakrishna <mr365@njit.edu>
2014-11-07 00:06:43 -0500

    installed bootstrap template

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8" />
-	<title> Mari Conference </title>
-</head>
-<body>
-<p>Welcome to the Mari Conference </p>
-</body>
+  <head>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="_/css/bootstrap.css" rel="stylesheet">
+    <link href="_/css/mystyles.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    
+    <script src="_/js/bootstrap.js"></script>
+     <script src="_/js/myscript.js"></script>
+  </body>
 </html>
