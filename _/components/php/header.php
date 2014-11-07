8de40f0c226599771bd5c8abdfefcd5370a5a73b
Mariprasad Ramakrishna <mr365@njit.edu>
2014-11-07 02:43:26 -0500

    corrected a link in the header.php file

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -2,7 +2,7 @@
 	<div class="col-lg-12">
 		<header class="clearfix">
 			<section id="branding">
-				<a href="index.php"> <img src="images/misc/brandlogo.jpg" alt="Logo for mari's conference 2014"></a>
+				<a href="index.php"> <img src="/images/misc/brandlogo.jpg" alt="Logo for mari's conference 2014"></a>
 			</section><!-- branding -->
 
 		</header><!-- header -->
