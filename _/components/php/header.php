bead4c222215c8db0bbfd51460ee6444e6a37f02
Mariprasad Ramakrishna <mr365@njit.edu>
2014-11-07 18:52:18 -0500

    made changes to js folder

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -1,6 +1,6 @@
 <div class="content row">
 	<div class="col-lg-12">
-		<header class="clearfix">
+		<header class="clearfix"><!-- to clear all floats -->
 			<section id="branding">
 				<a href="index.php"> <img src="/images/misc/brandlogo.jpg" alt="Logo for mari's conference 2014"></a>
 			</section><!-- branding -->
@@ -8,7 +8,7 @@
 			<section class="navbar">
 				<ul class="nav navbar-nav">
 					<li><a href="index.php">Home</a></li>
-					<li><a href="venuetravel.php">Venue</a></li>
+					<li class="active"><a href="venuetravel.php">Venue</a></li>
 				    <li><a href="schedule.php">Schedule</a></li>
 				    <li class="dropdown">
 				    	<a href="#" class="dropdown-toggle"data-toggle="dropdown">Artists <span class="caret"></span></a>
@@ -19,7 +19,7 @@
 				    	<li><a tableinex="-1" href="artists.php#SPBalu">S.P.Balu</a></li>
 				    	<li><a tableinex="-1" href="artists.php#PSusheela">P.Susheela</a></li>
 				    	<li><a tableinex="-1" href="artists.php#Illayaraja">Illayaraja</a></li>
-				    	</ul><!-- dropdown mwnu-->
+				    	</ul><!-- dropdown menu-->
 				    </li>
 				    <li><a href="register.php">Register</a></li>
 				</ul><!-- nav-->
