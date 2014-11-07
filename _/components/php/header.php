d528d6ab4dd05c3beb613201155996d34e219a7f
Mariprasad Ramakrishna <mr365@njit.edu>
2014-11-07 02:25:00 -0500

    added an image file- brand logo

---
+++ _/components/php/header.php
@@ -0,0 +1,11 @@
+<div class="content row">
+	<div class="col-lg-12">
+		<header class="clearfix">
+			<section id="branding">
+				<a href="index.php"> <img src="images/misc/brandlogo.jpg" alt="Logo for mari's conference 2014"></a>
+			</section><!-- branding -->
+
+		</header><!-- header -->
+	</div><!-- column -->
+
+</div><!-- content -->
