b78c721b0c12e1f4637830ca5285e791c3020596
Mariprasad Ramakrishna <mr365@njit.edu>
2014-11-14 14:50:06 -0500

    navBar not working on iphone6

--- _/components/php/article-venues.php
+++ _/components/php/article-venues.php
@@ -6,8 +6,8 @@
 <img class="pull-left img-rounded"  src="images/hotels/spa.jpg">
 
  <div class="media-body clearfix">
-			<p>A few minutes before 8 o’clock one Sunday evening last July, around 600 people crowded into the main conference hall of the South Point casino in Las Vegas. </p>
-			<p class="hidden-md hidden-xs" > Geller, who now lives in a large house beside the Thames River in England, says he long ago put the feud with Randi behind him. He claims to have used his show-business career as a cover for paranormal work on behalf of Mossad and the C.I.A., but he no longer calls himself a psychic. “I changed my title to ‘mystifier,’ ” he told me. “And I love it — because it means nothing.” But Randi’s contempt for him still burns brightly. “He knows he is deceiving these people — individuals, in most cases — and he doesn’t care what damage he does to them,” Randi said. “They depend on the paranormal after they have met Geller, and you cannot talk them out of it. And that has crippled them for life.”After taking their seats on red-velvet upholstered chairs, they chattered noisily as they awaited the start of the Million Dollar Challenge. When Fei Wang, a 32-year-old Chinese salesman, stepped onto the stage, they fell silent.When Fei Wang, a 32-year-old Chinese salesman, stepped onto the stage, they fell silent.</p>
+			<p>A few minutes before 8 o’clock one Sunday evening last July, around 600 people crowded into the main conference hall of the South Point casino in Las Vegas. Whenever it is said that he goes to the Thames River in England, says he long ago put the feud with Randi behind him.</p>
+			<p class="hidden-xs" > Geller, who now lives in a large house beside the Thames River in England, says he long ago put the feud with Randi behind him. </p>
 	</div>
 </section>
 <section class="venueinfo col-md-12 clearfix">
--- _/components/php/aside-abouttheartists.php
+++ _/components/php/aside-abouttheartists.php
@@ -1,7 +1,7 @@
 <aside class="abouttheartists">
 	<h2> About the artists</h2>
-		<p>If she is confirmed, her appointment will also allow the president, questioned in recent days about what he may do differently after his party’s thrashing, to bring a fresh face into an administration many have criticized as too insular.</p>
-		<p class="hidden-xs">The initial Republican reaction was guardedly positive. Senator Charles E. Grassley of Iowa, the senior Republican on the Judiciary Committee, said Ms. Lynch would receive “a very fair, but thorough, vetting” by the panel.</p>
+		<p>If she is confirmed, her appointment will also allow the president, questioned in recent days.</p>
+		<p class="hidden-xs">The initial Republican reaction was guardedly positive. </p>
 	<?php include "snippet-artistphotogrid.php"; ?>
 	<div style="padding-top:5px;" >
 	<a href="artists.php" class="btn btn-warning">about the artists</a>
